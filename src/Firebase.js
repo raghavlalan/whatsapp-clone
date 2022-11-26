1f95c1f44008749db138990010447767f8b592e9
Raghav Lalan kumar <raghavlalan0@gmail.com>
2022-11-26 14:18:05 +0530

    on progress

---
+++ src/Firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDmZYoNeAXUanG3GypuVDeNxtyri56oZP0",
+    authDomain: "whatsapp-clone-1cf13.firebaseapp.com",
+    projectId: "whatsapp-clone-1cf13",
+    storageBucket: "whatsapp-clone-1cf13.appspot.com",
+    messagingSenderId: "769282831292",
+    appId: "1:769282831292:web:8ee80f8dbf30289c9b31b9"
+  };
